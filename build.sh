a911e19f996fe6f4d59b208ca0b09c0edaf61892
ghjiee <9637301+ghjiee@user.noreply.gitee.com>
2024-11-22 15:48:01 +0800

    add defconfig

---
+++ build.sh
@@ -0,0 +1,11 @@
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- O=build licheepi_nano_defconfig
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- O=build saveconfig
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- O=build defconfig
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- O=build menuconfig
+
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- O=build -j16 zImage
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- O=build -j16 suniv-f1c100s-licheepi-nano.dtb
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- O=build -j16 modules
+
+rm -rf build/out
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- O=build  INSTALL_MOD_PATH=out modules_install -j16
